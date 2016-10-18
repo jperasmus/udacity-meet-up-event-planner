5a9a33bef5bf18cb2e4357e20d01fbda0297db0e
JP Erasmus <jperasmus11@gmail.com>
2016-10-18 17:26:49 +0200

    Basic footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,29 @@
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
-import './index.css';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  height: 80,
+  width: '100%',
+  padding: 15,
+  margin: 0,
+  fontSize: '.75rem',
+  textAlign: 'left',
+  display: 'block'
+};
 
 const Footer = (props) => {
   return (
-    <footer>
+    <Paper rounded={false} zDepth={0} style={style}>
       rendezvous
       ˈrɒndɪvuː,-deɪvuː/
-      noun
-      1.
-      a meeting at an agreed time and place.
-      "Edward turned up late for their rendezvous"
-      synonyms:	meeting, appointment, engagement
-    </footer>
+      <p>
+        noun
+        1.
+        a meeting at an agreed time and place.
+        "Edward turned up late for their rendezvous"
+        synonyms:	meeting, appointment, engagement
+      </p>
+    </Paper>
   );
 };
 
